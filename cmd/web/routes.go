5abdfa0a87c57e18036e6536d7caffea1931316f
unsubstantiated-Script <jusfulton@gmail.com>
2022-08-06 08:36:27 -0700

    adding images to project

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -24,5 +24,9 @@ func routes(app *config.AppConfig) http.Handler {
 	//mux.Use(WriteToConsole)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	//accessing static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
