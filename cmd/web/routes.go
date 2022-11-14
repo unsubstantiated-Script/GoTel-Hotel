d259fb1d4348e0266d1707c1ee7f66ebb34670ba
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2022-11-13 17:15:43 -0800

    Initial routes and handler setup for form validation CSRFToken as well

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -27,6 +27,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Post("/make-reservation", handlers.Repo.PostReservation)
 	//accessing static files
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
